ca0027342c0a238681d420d32bf362beae39b48f
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2020-05-24 23:28:06 +0200

    Initial commit.

---
+++ app/src/main/java/com/example/gdgandroidwebinar4/ui/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.gdgandroidwebinar4.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.gdgandroidwebinar4.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+}
